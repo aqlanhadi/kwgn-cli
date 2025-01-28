a7bf6e3a9beb0b151dc8fcf8264e911c2d8be4e7
Aqlan Nor Azman <aqlanhadi@gmail.com>
2025-01-28 16:29:36 +0800

    feat: extract text by rows

--- extractor/common/utils.go
+++ extractor/common/utils.go
@@ -1,22 +1,37 @@
 package common
 
 import (
-	"bytes"
-
 	"github.com/dslipak/pdf"
 )
 
-func ExtractTextFromPDF(path string) (string, error) {
-	f, err := pdf.Open(path)
+func ExtractRowsFromPDF(path string) ([]string, error) {
+	r, err := pdf.Open(path)
 	// remember close file
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	var buf bytes.Buffer
-    b, err := f.GetPlainText()
-    if err != nil {
-        return "", err
-    }
-    buf.ReadFrom(b)
-	return buf.String(), nil
+
+	extracted_rows := []string{}
+	
+	for no := 1; no < r.NumPage(); no++ { // Loop over each page.
+		page := r.Page(no)
+		rows, _ := page.GetTextByRow()
+		for _, row := range rows { // Loop over each row of text in the page.
+
+			// concatenate all text in the row
+			var rowText string
+			for _, text := range row.Content {
+				rowText += text.S + " "
+			}
+
+			// fmt.Println("Page", no, "Row", ri, "Text", rowText)
+			extracted_rows = append(extracted_rows, rowText)
+		}
+	}
+
+	// fmt.Println("Extracted rows: ", extracted_rows)
+
+	return extracted_rows, nil
+    // buf.ReadFrom(b)
+	// return buf.String(), nil
 }
